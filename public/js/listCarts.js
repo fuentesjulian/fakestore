d80518b757651628c7fc5ab685f87955d52c6771
fuentesjulian <fuentes.julian@hotmail.com>
2022-08-29 21:21:14 -0300

    Adding admin view of all carts

--- public/js/listCarts.js
+++ public/js/listCarts.js
@@ -1,8 +1,20 @@
 import { cartApi } from "./cartApi.js";
 
+const carts = document.getElementById("carts");
 const getData = async () => {
   const data = await cartApi.getIds();
-  console.log(data);
+  let cartHtml = "";
+  data.forEach((cart) => {
+    let prodHtml = "";
+
+    if (cart.products?.length > 0) {
+      cart.products.forEach((product) => {
+        prodHtml += `<p>ID ${product.id} - ${product.name} - ${product.quantity} unidades - $${product.price} por unidad</p>`;
+      });
+    }
+    cartHtml += `<h5>Cart Id ${cart.id} - timestamp ${cart.timestamp}</h5>${prodHtml}`;
+  });
+  carts.innerHTML = cartHtml;
 };
 
 /* corro la funcion de actualizarHTML cuando carga el DOM */
