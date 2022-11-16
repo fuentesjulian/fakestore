33965e39041c24d4cb4310cb97f084b198ce0355
fuentesjulian <fuentes.julian@hotmail.com>
2022-11-16 08:04:24 -0300

    Agregando capacidad de correr como cluster y agregando la opcion de elegir puerto y modo de ejecucion como argumento via yargs

--- src/main.js
+++ src/main.js
@@ -1,9 +1,61 @@
 // importo server.js
-import app from "./server.js"
-// declaro el puerto
-const PORT = process.env.PORT || 8080;
+import app from "./server.js";
+import Yargs from "yargs";
+import cluster from "cluster";
+import os from "os";
+
+const numCPUs = os.cpus().length;
+
+//--------------------------------------------
+// funcion runCluster que corre el servidor como cluster
+// toma como parametro el puerto
+const runCluster = (port) => {
+  if (cluster.isPrimary) {
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+  } else {
+    // corro app
+    const server = app.listen(port, () => {
+      console.log(
+        `Servidor http escuchando en el puerto ${server.address().port} en modo cluster`
+      );
+    });
+    server.on("error", (error) => console.log(`Error en servidor ${error}`));
+  }
+};
+
+//--------------------------------------------
+// funcion runCluster que corre el servidor normalmente
+// toma como parametro el puerto
+const runNormal = (port) => {
+  // corro app
+  const server = app.listen(port, () => {
+    console.log(
+      `Servidor http escuchando en el puerto ${server.address().port} en modo normal`
+    );
+  });
+  server.on("error", (error) => console.log(`Error en servidor ${error}`));
+};
+
 // corro app
-const server = app.listen(PORT, () => {
-  console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
-});
-server.on("error", (error) => console.log(`Error en servidor ${error}`));
+//--------------------------------------------
+// inicio el servidor
+const yargs = Yargs(process.argv.slice(2));
+const args = yargs
+  .alias({ p: "port", m: "mode" })
+  .default({ port: 8080, mode: "NORMAL" }).argv;
+
+const PORT = process.env.PORT || args.port || 8080;
+const MODE = args.mode;
+
+switch (MODE) {
+  case "CLUSTER":
+    runCluster(PORT);
+    break;
+
+  case "NORMAL":
+    runNormal(PORT);
+    break;
+  default:
+}
